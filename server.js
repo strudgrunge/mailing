92713c45608ebb4149513b35459a06949f437699
Julio Villar <jvdesarrollo@gmail.com>
2023-04-02 19:51:23 -0300

    Comments erased

--- server.js
+++ server.js
@@ -17,29 +17,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/contactform.html')
-// })
-
-// app.post('/', (req, res) => {
-//     console.log('req.body', req.body);
-//     const transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth: {
-//             user: 'rurosurunironin1@gmail.com',
-//             pass: 'potiynyuodeyhirw',
-//         },
-//     });
-//     transporter.sendMail({
-//         from: req.body.email, // sender address
-//         to: "jvdesarrollo@gmail.com", // list of receivers
-//         subject: `Message from ${req.body.name}: ${req.body.subject}`, // Subject line
-//         text: req.body.message, // plain text body
-//         // html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
-//     }).then(info => {
-//         console.log({ info });
-//     }).catch(console.error);
-// })
 
 app.use('/send', sendMailRoutes)
 
